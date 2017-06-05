84a349ae1b3a5d0f85f9addb3cb75a5195d5b544
razmik <razm89@yandex.ru>
2017-06-04 17:01:32 -0700

    Initial commit

---
+++ app/src/main/java/com/guthub/duderazm/hellouser/MainActivity.java
@@ -0,0 +1,15 @@
+package com.guthub.duderazm.hellouser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
